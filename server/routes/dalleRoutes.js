7529ddaf71a0a16a75928bfa88695ca7eae75d79
Raj Singh <rajsinghast06@gmail.com>
2023-03-30 22:57:55 +0530

    Completed fetching part

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -7,30 +7,32 @@ dotenv.config();
 const router = express.Router();
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
 router.route("/").get((req, res) => {
-    res.send("Hello from openAI");
+  res.status(200).json({ message: "Hello from DALL-E!" });
 });
 
 router.route("/").post(async (req, res) => {
-    try {
-        const { prompt } = req.body;
-        const altResponse = await openai.createImage({
-            prompt,
-            n: 1,
-            size: "1024x1024",
-            response_format: "b64_json",
-        })
-        const image = altResponse.data.data[0].b64_json;
-        res.status(200).json({ photo: image });
-    } catch (error) {
-        console.log(error);
-        res.send(500).send(err?.response.data.error.message);
-    }
-})
+  try {
+    const { prompt } = req.body;
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+    const image = aiResponse.data.data[0].b64_json;
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send(error?.response.data.error.message || "Something went wrong");
+  }
+});
 
 export default router;
